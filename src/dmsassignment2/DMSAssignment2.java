e7dc9c1eea2a3489cdeb3d12039cf702b4886d06
Logan Martlew <logan.martlew@gmail.com>
2022-04-16 16:33:48 +1200

    Added vector timestamp class

--- src/dmsassignment2/DMSAssignment2.java
+++ src/dmsassignment2/DMSAssignment2.java
@@ -4,6 +4,8 @@
  */
 package dmsassignment2;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author logan
@@ -16,6 +18,32 @@ public class DMSAssignment2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
+        
+        VectorTimestamp timestamp = new VectorTimestamp(4);
+        
+        ArrayList<Integer> oldVector = new ArrayList();
+        oldVector.add(5);
+        oldVector.add(7);
+        oldVector.add(54);
+        oldVector.add(8);
+        oldVector.add(4);
+        timestamp.setVector(oldVector);
+        
+        System.out.println(timestamp.getVector());
+        
+        ArrayList<Integer> newVector = new ArrayList();
+        newVector.add(3);
+        newVector.add(9);
+        newVector.add(55);
+        newVector.add(1);
+        newVector.add(1);
+        
+        try {
+            timestamp.update(newVector);
+        
+            System.out.println(timestamp.getVector());
+        } catch (Exception e) {}
+        
     }
     
 }
---
+++ src/dmsassignment2/VectorTimestamp.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dmsassignment2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Logan
+ */
+public class VectorTimestamp {
+    private int index;
+    private ArrayList<Integer> vector;
+    
+    public VectorTimestamp(int index) {
+        this.index = index;
+        this.vector = new ArrayList();
+        
+        for (int i = 0; i <= index; i++) {
+            this.vector.add(0);
+        }
+        
+        System.out.println(vector);
+    }
+    
+    public void addMember() {
+        this.vector.add(0);
+    }
+    
+    public void eventOccurred() {
+        this.vector.set(this.index, this.vector.get(this.index) + 1);
+    }
+    
+    public ArrayList<Integer> getVector() {
+        return this.vector;
+    }
+    
+    // For testing purposes
+    public void setVector(ArrayList<Integer> vector) {
+        this.vector = vector;
+    }
+    
+    public void update(ArrayList<Integer> recievedVector) throws Exception {
+        if (recievedVector.size() != this.vector.size()) {
+            throw new Exception("Vector timestamps were not of the same size");
+        }
+        
+        for (int i = 0; i < recievedVector.size(); i++) {
+            if (recievedVector.get(i) > this.vector.get(i)) {
+                this.vector.set(i, recievedVector.get(i));
+            }
+        }
+    }
+}
