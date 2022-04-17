e8dc6837f68a2d0a389e73556ed7c8f114e66f4c
Logan Martlew <logan.martlew@gmail.com>
2022-04-17 16:51:57 +1200

    RMI connections working

--- src/dmsassignment2/DMSAssignment2.java
+++ src/dmsassignment2/DMSAssignment2.java
@@ -6,12 +6,16 @@ package dmsassignment2;
 
 
 import java.net.MalformedURLException;
+import java.rmi.AccessException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import static java.util.Arrays.asList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -23,28 +27,50 @@ public class DMSAssignment2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Registry registry = connectToRegistry();
+    }
+
+    private static Registry connectToRegistry() {
         Registry registry;
+        
         try {
             LocateRegistry.createRegistry(1099);
         } catch (RemoteException e) {
             System.out.println("Can't create a new registry");
         }
         
-        try {  // create stub (note prior to Java 5.0 must use rmic utility)
-            // get the registry which is running on the default port 1099
+        try {
             registry = LocateRegistry.getRegistry();
             
-            if(asList(registry.list()).contains(PeerConnectionsImpl.NAME)){
+            // Ensures a PeerConnections object is in registry
+            if (!asList(registry.list()).contains(PeerConnectionsImpl.NAME)) {
+                PeerConnectionsImpl remoteObject = new PeerConnectionsImpl();
                 
+                PeerConnections stub = 
+                    (PeerConnections) UnicastRemoteObject.exportObject(remoteObject, 0);
+                registry.rebind(PeerConnectionsImpl.NAME, stub);
             }
             
+            long processID = ProcessHandle.current().pid();
+            
+            PeerConnections connections;
+            try {
+                connections = (PeerConnections) registry.lookup(PeerConnectionsImpl.NAME);
+                connections.addPeer(Long.toString(processID));
+                
+                System.out.println("Connected with process id: " + processID);
+                System.out.println(connections.getPeers());
+            } catch (NotBoundException ex) {
+                System.out.println("PeerConnections object not found");
+            } catch (AccessException ex) {
+                System.out.println(ex.getMessage());
+            }
+            
+            return registry;
         } catch (RemoteException e) {
             System.err.println("Unable to bind to registry: " + e);
-            return;
         }
         
-        // note that separate thread created to keep remoteObject alive
-        System.out.println("Main method of DMSAssignment2 done.");
+        return null;
     }
-
 }
