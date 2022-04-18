4f5c347cba7f5a60b8175cb8703212917110169a
Woccz <44993506+Woccz@users.noreply.github.com>
2022-04-18 18:50:02 +1200

    complete tree construction

--- src/dmsassignment2/TokenTreeNode.java
+++ src/dmsassignment2/TokenTreeNode.java
@@ -17,6 +17,8 @@ public class TokenTreeNode {
         RIGHT,
         HERE
     }
+
+    public final String pid;
     
     private TokenTreeNode parent = null;
     private TokenTreeNode left = null;
@@ -25,12 +27,34 @@ public class TokenTreeNode {
     private TokenLocation tokenLocation = TokenLocation.ABOVE;
     private boolean accessRequested = false;
     
-    public TokenTreeNode() {}
+    public TokenTreeNode(String pid) {
+        this.pid = pid;
+    }
     
-    public void constructFullTree(List<TokenTreeNode> nodes, TokenTreeNode parent) {
+    public void constructFullTree(TokenTreeNode parent, List<TokenTreeNode> nodes) {
         if (parent == null) {
             this.tokenLocation = TokenLocation.HERE;
+            // sort nodes by pid in descending order
+            nodes.sort((TokenTreeNode n1, TokenTreeNode n2) -> {
+                return n2.pid.compareTo(n1.pid);
+            });
+        }   // else location remains ABOVE
+
+        if (nodes.size() == 0) {    // leaf node
+            return;
         }
+
+        if (nodes.size() == 1) {    // single node
+            this.left = nodes.get(0);
+            this.left.constructFullTree(parent, nodes.subList(1, nodes.size()));
+            return;
+        }
+
+        // if there is a left and a right node, then set left node as left child and right node as right child and pass each half of the remaining nodes
+        this.left = nodes.get(0);
+        this.left.constructFullTree(this, nodes.subList(1, nodes.size() / 2 + 1));
+        this.right = nodes.get(nodes.size() / 2 + 1);
+        this.right.constructFullTree(this, nodes.subList(nodes.size() / 2 + 2, nodes.size()));
         
         
     }
