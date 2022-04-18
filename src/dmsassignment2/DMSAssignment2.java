6f9cc4ccd9b6976c7ae3844fc5948b28c520ae57
Woccz <44993506+Woccz@users.noreply.github.com>
2022-04-18 17:19:00 +1200

    Added User commands for Biographies

--- src/dmsassignment2/DMSAssignment2.java
+++ src/dmsassignment2/DMSAssignment2.java
@@ -168,6 +168,10 @@ public class DMSAssignment2 {
         }
     }
     
+    public static boolean isConnected() {
+        return connected;
+    }
+    
     
     // ----- User command methods ----- \\
     
@@ -221,6 +225,10 @@ public class DMSAssignment2 {
         return true;
     }
     
+    public static void setBio(String newBio) {
+        // TODO: set current user bio
+    }
+    
     public static String getBio(String fetchUsername){
         // TODO: Implement RMI get user biography
         return "";
@@ -228,6 +236,11 @@ public class DMSAssignment2 {
     
     public static void rateBio(String username, int rating){
         // TODO: Implement token ring syncronised rate biography with +1 or -1
+    }
+    
+    public static int getBioRating(String fetchUsername) {
+        // TODO get the bio rating for the selected user
+        return 0;
     }
     
     public static String takeSnapshot(){
